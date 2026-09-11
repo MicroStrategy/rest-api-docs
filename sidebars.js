b1f95a0a2dc44dd22c045dcec4d8a545154aab14
jqian-mstr <jqian@microstrategy.com>
2026-09-11 10:55:42 +0800

    Revert "26.09 (#83)" (#84)
    This reverts commit 98d31e5d83f3a6884f05f0cdc946b9a8a145a3cc.

--- sidebars.js
+++ sidebars.js
@@ -1269,7 +1269,6 @@ const sidebars = {
               },
               items: [
                 "common-workflows/mosaic/retrieve-data-from-data-models/retrieve-data-through-cube-instance",
-                "common-workflows/mosaic/retrieve-data-from-data-models/apply-parameters-to-a-data-model-instance",
                 "common-workflows/mosaic/retrieve-data-from-data-models/retrieve-data-model-sql-view",
               ],
             },
